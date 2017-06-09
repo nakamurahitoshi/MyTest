d63c110638ca569bfef448601e70471804cbde03
HitoshiNakamura <HitoshiNakamura@DESKTOP-NL6T18E>
2017-06-09 14:04:45 +0900

    TestClass2にTestProjectプロジェクトから変更を加えた

--- src/testpackage/TestClass2.java
+++ src/testpackage/TestClass2.java
@@ -3,5 +3,6 @@ package testpackage;
 public class TestClass2 {
 	public static void main(String[] args) {
 		System.out.println("TestClass, No.2");
+		System.out.println("TestClass, mod14:03");
 	}
 }
