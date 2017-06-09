eb816c32716c135aa6336e14b59c5e02d5c5d635
HitoshiNakamura <HitoshiNakamura@DESKTOP-NL6T18E>
2017-06-09 12:42:18 +0900

    TestClassにコンソール出力を1行追加． 新しいクラスTestClass2を作成．

--- src/testpackage/TestClass.java
+++ src/testpackage/TestClass.java
@@ -1,6 +1,6 @@
 package testpackage;
 /**
- * 
+ *
  * @author HitoshiNakamura
  *
  */
@@ -8,7 +8,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 	System.out.println("test");
-
+	System.out.println("test and modified");
 	}
 
 }
